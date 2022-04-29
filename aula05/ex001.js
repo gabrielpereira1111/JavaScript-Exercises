83eb6ad3d163196a26f5280889a45505a9115d9c
Gabriel Pereira <gabriel_pereira02@outlook.com.br>
2022-04-29 18:49:56 -0300

    .

---
+++ aula05/ex001.js
@@ -0,0 +1,25 @@
+// Variáveis e Tipos Primitivos
+
+/*
+
+    Variáveis são espaços na memórias que podem receber diferentes tipos de valores.
+
+    Os tipos de variáveis são 'var' e 'let'. O 'var' tem escopo global, ou seja, funciona em toda a aplicação. O 'let' funciona apenas em no escopo onde ele é declarado, ou seja, em apenas uma parte especifica do código. Por exemplo, se um 'let' for usado em uma função, ele apenas funcionará dentro dela.
+
+    Os principais tipos primitivos são: boolean, string e number.
+    String -> cadeias de caracteres, declaradas entre aspas(""; ''; ´´)
+    Number -> números em geral
+    boolean -> valores false ou true
+
+    typeof serve para verificar o tipo primitivo da variável
+
+*/ 
+
+var a;
+let b;
+
+a = 25;
+b = "Gabriel"
+
+console.log(typeof a)
+console.log(typeof b)
